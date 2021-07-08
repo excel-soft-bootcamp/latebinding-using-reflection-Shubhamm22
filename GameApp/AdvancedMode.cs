feb28d84c1b08b7fe67afc343c94f3a140c30726
Shubhamm22 <86050595+Shubhamm22@users.noreply.github.com>
2021-07-08 09:50:25 +0530

    update

---
+++ GameApp/AdvancedMode.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameApp
+{
+    class AdvancedMode
+    {
+        public void Load(string pathname, string classname, string methodname, string playername, int points)
+        {
+            System.Reflection.Assembly nameLib =
+              System.Reflection.Assembly.LoadFile(pathname);
+            System.Type nameClass = nameLib.GetType(classname);
+            if (nameClass != null)
+            {
+                if (nameClass.IsClass)
+                {
+                    Object objRef = System.Activator.CreateInstance(nameClass);
+                    System.Reflection.MethodInfo _methodRef = nameClass.GetMethod(methodname);
+                    if (!_methodRef.IsStatic)
+                    {
+                        object advanceResult = _methodRef.Invoke(objRef, new object[] {(playername, points) });
+                        Console.WriteLine(advanceResult.ToString());
+                    }
+                }
+
+            }
+        }
+    }
+}
--- GameApp/Program.cs
+++ GameApp/Program.cs
@@ -6,64 +6,67 @@ using System.Threading.Tasks;
 
 namespace GameApp
 {
-    //Named Constants
-    //Constant Value is - int
     public enum Options
     {
-        BASIC=1,INTERMEDIATE,ADVANCED
+        BASIC = 1, INTERMEDIATE, ADVANCED
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Word Guess Game");
-            
-            string message = string.Format("Enter Your Choice {0}->Basic , {1}->Intermediate ,{2}->Advanced",Options.BASIC,Options.INTERMEDIATE,Options.ADVANCED);// 1->Basic,2->Intermediate
-            //String Interpollation 
-            string displayMessage = $"Enter Your Choice {(int)Options.BASIC}->Basic,{(int)Options.INTERMEDIATE}->Intermediate,{(int)Options.ADVANCED}->Advanced";
-            Console.WriteLine(displayMessage);
-            Options _choice =(Options)Int32.Parse( Console.ReadLine());
-            switch (_choice)
-            {
-                case Options.BASIC:
-                    Console.WriteLine("Basic Level");
-                    //Use Reflection  
-                    //Assembly Load
-                    System.Reflection.Assembly basicLevelLib = 
-      System.Reflection.Assembly.LoadFile(@"C:\Users\user\source\repos\excelsoft\Examples\GameApp\bin\Debug\LevelLibs\BasicLevelLib.dll");
-                    // Search For Class - BasicLevelType
-                   System.Type basicLevelTypeClassRef= basicLevelLib.GetType("BasicLevelLib.BasicLevelType");
-                    if (basicLevelTypeClassRef != null)
-                    {
-                        if (basicLevelTypeClassRef.IsClass)
-                        {
-                            //Instantiate Type
-                            //BasicLevelLib.BasicLevelType objref=new BasicLevelLib.BasicLevelType() ; Early Binding
-                           Object objRef =System.Activator.CreateInstance(basicLevelTypeClassRef); //LateBinding Code
-                            //Discove Method
-                          System.Reflection.MethodInfo _methodRef=  basicLevelTypeClassRef.GetMethod("Play");
-                            if (!_methodRef.IsStatic)
-                            {
-                                //Invoke NonStatic Method
-                                // string Play(string playerName, int earlierPoints){}
-                                //object result=  _methodRef.Invoke(objRef, new object[] {"Tom",20 });
-                                object result =  _methodRef.Invoke(objRef, new object[] {});
-                                Console.WriteLine(result.ToString());
-                            }
 
-                        }
+            Console.WriteLine("Word Guess Game");
+
+            string message = string.Format("Enter Your Choice {0}->Basic , {1}->Intermediate ,{2}->Advanced", Options.BASIC, Options.INTERMEDIATE, Options.ADVANCED);// 1->Basic,2->Intermediate
+            Options _choice = default;
+
+
+            int attempts = 3;
+            for (int i = 0; i <= attempts; i++)
+            {
+                do
+                {
+
+
+                    string displayMessage = $"Enter Your Choice {(int)Options.BASIC}->Basic,{(int)Options.INTERMEDIATE}->Intermediate,{(int)Options.ADVANCED}->Advanced";
+                    Console.WriteLine(displayMessage);
+
+                    _choice = (Options)Int32.Parse(Console.ReadLine());
+
+
+
+                    switch (_choice)
+                    {
+
+                        case Options.BASIC:
+                            Console.WriteLine("Basic Level");
+                            BasicMode _basicMode = new BasicMode();
+                            _basicMode.Load(@"C:\Users\shubham.m\Downloads\download\BasicLevelLib.dll", "BasicLevelLib.BasicLevelType", "Play");
+                            break;
+                        case Options.INTERMEDIATE:
+                            Console.WriteLine("Intermediate Level");
+                            IntermediateMode _intermediateMode = new IntermediateMode();
+                            _intermediateMode.Load(@"C:\Users\shubham.m\Downloads\download\IntermediateLevelLib.dll", "IntermediateLevelLib.IntermediateLevelType", "Start", "Shubham");
+
+                            break;
+                        case Options.ADVANCED:
+                            Console.WriteLine("Advanced Level");
+                            AdvancedMode _advanceMode = new AdvancedMode();
+                            _advanceMode.Load(@"C:\Users\shubham.m\Downloads\download\AdvancedLevelLib.dll", "AdvancedLevelLib.AdvancedLevelType", "Begin", "Shubham", 50);
+
+                            break;
+                        default:
+                            Console.WriteLine("Invalid Input");
+
+                            break;
 
                     }
-                     break;
-                case Options.INTERMEDIATE:
-                    Console.WriteLine("Intermediate Level");
-                     break;
-                case Options.ADVANCED:
-                    Console.WriteLine("Advanced Level");
+                    attempts--;
                     break;
-                
-            }
 
+                } while (_choice != Options.BASIC && _choice != Options.INTERMEDIATE && _choice != Options.ADVANCED);
+            }
         }
     }
 }
+
